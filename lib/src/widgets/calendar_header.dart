14d529e54950f1f4c8b0a06c2d792ec39544ca3f
Khalid CHAMAKH <k.chamakh@ayouris.com>
2024-01-08 13:38:55 +0100

    Initial commit

---
+++ lib/src/widgets/calendar_header.dart
@@ -0,0 +1,176 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+import 'package:nawat_mobile/app/config/TextStyle.dart';
+import 'package:nawat_mobile/app/config/theme/AppTheme.dart';
+
+import '../customization/header_style.dart';
+import '../shared/utils.dart' show CalendarFormat, DayBuilder;
+
+class CalendarHeader extends StatelessWidget {
+  final dynamic locale;
+  final DateTime focusedMonth;
+  final CalendarFormat calendarFormat;
+  final HeaderStyle headerStyle;
+  final VoidCallback onLeftChevronTap;
+  final VoidCallback onRightChevronTap;
+  final VoidCallback onHeaderTap;
+  final VoidCallback onHeaderLongPress;
+  final ValueChanged<CalendarFormat> onFormatButtonTap;
+  final Map<CalendarFormat, String> availableCalendarFormats;
+  final DayBuilder? headerTitleBuilder;
+
+  const CalendarHeader({
+    Key? key,
+    this.locale,
+    required this.focusedMonth,
+    required this.calendarFormat,
+    required this.headerStyle,
+    required this.onLeftChevronTap,
+    required this.onRightChevronTap,
+    required this.onHeaderTap,
+    required this.onHeaderLongPress,
+    required this.onFormatButtonTap,
+    required this.availableCalendarFormats,
+    this.headerTitleBuilder,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final text = headerStyle.titleTextFormatter?.call(focusedMonth, locale) ??
+        DateFormat.yMMMM(locale).format(focusedMonth);
+
+    return Container(
+
+      padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: onLeftChevronTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: AppThemeConfig().backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: AppThemeConfig().autoCompleteBorder),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset("assets/icons/chevron_left_duo.svg",color: AppThemeConfig().titleColor,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 4,),
+                GestureDetector(
+                  onTap: onLeftChevronTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: AppThemeConfig().backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: AppThemeConfig().autoCompleteBorder),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset("assets/icons/chevron_left.svg",color: AppThemeConfig().titleColor,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              text,
+              style: CustomTextStyle().drawerTitle,
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              children: [
+                GestureDetector(
+                  onTap: onRightChevronTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: AppThemeConfig().backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: AppThemeConfig().autoCompleteBorder),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset("assets/icons/chevron_right.svg", width: 20, height: 20,color: AppThemeConfig().titleColor,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 4,),
+                GestureDetector(
+                  onTap: onRightChevronTap,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: ShapeDecoration(
+                      color: AppThemeConfig().backgroundColor,
+                      shape: RoundedRectangleBorder(
+                        side: BorderSide(width: 1, color: AppThemeConfig().autoCompleteBorder),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 20,
+                          height: 20,
+                          child: SvgPicture.asset("assets/icons/chevron_right_duo.svg", width: 20, height: 20,color: AppThemeConfig().titleColor,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/src/widgets/cell_content.dart
@@ -0,0 +1,200 @@
+// Copyright 2019 Aleksander Woźniak
+// SPDX-License-Identifier: Apache-2.0
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:nawat_mobile/app/config/TextStyle.dart';
+import 'package:nawat_mobile/app/config/theme/AppTheme.dart';
+
+import '../customization/calendar_builders.dart';
+import '../customization/calendar_style.dart';
+
+class CellContent extends StatelessWidget {
+  final DateTime day;
+  final DateTime focusedDay;
+  final dynamic locale;
+  final bool isTodayHighlighted;
+  final bool isToday;
+  final bool isSelected;
+  final bool isRangeStart;
+  final bool isRangeEnd;
+  final bool isWithinRange;
+  final bool isOutside;
+  final bool isDisabled;
+  final bool isHoliday;
+  final bool isWeekend;
+  final CalendarStyle calendarStyle;
+  final CalendarBuilders calendarBuilders;
+
+  const CellContent({
+    Key? key,
+    required this.day,
+    required this.focusedDay,
+    required this.calendarStyle,
+    required this.calendarBuilders,
+    required this.isTodayHighlighted,
+    required this.isToday,
+    required this.isSelected,
+    required this.isRangeStart,
+    required this.isRangeEnd,
+    required this.isWithinRange,
+    required this.isOutside,
+    required this.isDisabled,
+    required this.isHoliday,
+    required this.isWeekend,
+    this.locale,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final dowLabel = DateFormat.EEEE(locale).format(day);
+    final dayLabel = DateFormat.yMMMMd(locale).format(day);
+    final semanticsLabel = '$dowLabel, $dayLabel';
+
+    Widget? cell =
+        calendarBuilders.prioritizedBuilder?.call(context, day, focusedDay);
+
+    if (cell != null) {
+      return Semantics(
+        label: semanticsLabel,
+        excludeSemantics: true,
+        child: cell,
+      );
+    }
+
+    final text = '${day.day}';
+    final margin = calendarStyle.cellMargin;
+    final padding = calendarStyle.cellPadding;
+    final alignment = calendarStyle.cellAlignment;
+    final duration = const Duration(milliseconds: 250);
+
+    if (int.parse(text)>DateTime.now().day) {
+      cell = calendarBuilders.disabledBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: calendarStyle.disabledDecoration,
+            alignment: alignment,
+            child: Text(text,
+              style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,color: AppThemeConfig().iconPrimary),
+            ),
+          );
+    } else if (isSelected) {
+      cell = calendarBuilders.selectedBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: ShapeDecoration(
+              color: AppThemeConfig().colorBlue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            ),            alignment: alignment,
+            child: Text(text, style: calendarStyle.selectedTextStyle),
+          );
+    } else if (isRangeStart) {
+      cell =
+          calendarBuilders.rangeStartBuilder?.call(context, day, focusedDay) ??
+              AnimatedContainer(
+                duration: duration,
+                margin: margin,
+                padding: padding,
+                decoration: ShapeDecoration(
+                  color: AppThemeConfig().colorBlue,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                ),
+                alignment: alignment,
+                child: Text(text,
+                  style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,color: Colors.white),
+                ),
+              );
+    } else if (isRangeEnd) {
+      cell = calendarBuilders.rangeEndBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: ShapeDecoration(
+              color: AppThemeConfig().colorBlue,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            ),
+            alignment: alignment,
+            child: Text(text,
+              style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,color: Colors.white),
+            ),
+          );
+    } else if (isToday && isTodayHighlighted) {
+      cell = calendarBuilders.todayBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: ShapeDecoration(
+              color: AppThemeConfig().backgroundColor,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            ),
+            alignment: alignment,
+            child: Text(text, style: TextStyle(
+              color: AppThemeConfig().titleColor,
+              fontSize: 16,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w500,
+            ),),
+          );
+    } else if (isHoliday) {
+      cell = calendarBuilders.holidayBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: calendarStyle.holidayDecoration,
+            alignment: alignment,
+            child: Text(text, style: calendarStyle.holidayTextStyle),
+          );
+    } else if (isWithinRange) {
+      cell =
+          calendarBuilders.withinRangeBuilder?.call(context, day, focusedDay) ??
+              AnimatedContainer(
+                duration: duration,
+                margin: margin,
+                padding: padding,
+                decoration: calendarStyle.withinRangeDecoration,
+                alignment: alignment,
+                child: Text(text,
+                  style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,color: AppThemeConfig().colorBlue),
+                ),
+              );
+    } else if (isOutside) {
+      cell = calendarBuilders.outsideBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            decoration: calendarStyle.disabledDecoration,
+            alignment: alignment,
+            child: Text(text,
+              style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,color: AppThemeConfig().iconPrimary),
+            ), );
+    } else {
+      cell = calendarBuilders.
+      defaultBuilder?.call(context, day, focusedDay) ??
+          AnimatedContainer(
+            duration: duration,
+            margin: margin,
+            padding: padding,
+            alignment: alignment,
+            child: Text(
+              text,
+              style: CustomTextStyle().drawerTitle.copyWith(fontWeight: FontWeight.w500,),
+            ),
+          );
+    }
+
+    return Semantics(
+      label: semanticsLabel,
+      excludeSemantics: true,
+      child: cell,
+    );
+  }
+}
